e524fea5f7db00a53b57e4d881a6e70c13c7a21d
Suganyabalasubramanian <suganbala597@gmail.com>
2021-05-26 10:28:51 +0530

    changes

--- resources/views/salesorder/create.blade.php
+++ resources/views/salesorder/create.blade.php
@@ -119,62 +119,7 @@ $fin_year=DB::table('financial_year')
                                             <td>Actual</td>
                                         </tr>
                                     </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td id="div" name="div"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                            <td id="text" name="text"></td>
-                                        </tr>
-                                        <tr>
-                                            <td id="total1" name="total1">Total</td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="text" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-                                            <td id="total" name="total"></td>
-
-                                        </tr>
+                                    <tbody name='abc' id='abc'>
 
                                     </tbody>
                                 </table>
@@ -182,7 +127,11 @@ $fin_year=DB::table('financial_year')
                             </div>
                         </div>
                     </div><br>
-
+                    <div class="form-group row">
+                        <div class="col-sm-12">
+                            <!-- <button type="button" class="btn btn-primary btn-sm" id="tot">Total</button> -->
+                        </div>
+                    </div>
 
                     <div class="form-group row">
 
@@ -237,48 +186,221 @@ $('#add').click(function() {
         },
         dataType: 'json',
         success: function(data) {
-            $('td[name="div"]').empty();
             $.each(data, function(key, value) {
-                $('td[name="div"]').append('<option value="' + key + '">' + value);
-                $('td[name="text"]').append(
-                    '<input type="text" style="width:60px" name="apr_target" id="apr_target" >'
-                );
+                $('tbody[name="abc"]').append('<tr>' + '<td>' + '<option value="' + key +
+                    '">' + value + '</td>' + '<td class="apt">' +
+                    '<input type="text" style="width:60px" name="apr_target" id="apr_target" class="apr_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="apr_actual" id="apr_actual" class="apr_actual" disabled>' +
+                    '</td>' + '<td class="may">' +
+                    '<input type="text" style="width:60px" name="may_target" id="may_target" class="may_target" >' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="may_actual" id="may_actual" class="may_actual" disabled>' +
+                    '</td>' + '<td class="june">' +
+                    '<input type="text" style="width:60px" name="june_target" id="june_target" class="june_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="june_actual" id="june_actual" class="june_actual" disabled>' +
+                    '</td>' + '<td class="july">' +
+                    '<input type="text" style="width:60px" name="july_target" id="july_target" class="july_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="july_actual" id="july_actual" class="july_actual" disabled>' +
+                    '</td>' + '<td class="aug">' +
+                    '<input type="text" style="width:60px" name="aug_target" id="aug_target" class="aug_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="aug_actual" id="aug_actual" class="aug_actual" disabled>' +
+                    '</td>' + '<td class="sept">' +
+                    '<input type="text" style="width:60px" name="sept_target" id="sept_target" class="sept_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="sept_actual" id="sept_actual" class="sept_actual" disabled>' +
+                    '</td>' + '<td class="oct">' +
+                    '<input type="text" style="width:60px" name="oct_target" id="oct_target" class="oct_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="oct_actual" id="oct_actual" class="oct_actual" disabled>' +
+                    '</td>' + '<td class="nov">' +
+                    '<input type="text" style="width:60px" name="nov_target" id="nov_target" class="nov_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="nov_actual" id="nov_actual" class="nov_actual" disabled>' +
+                    '</td>' + '<td class="dec">' +
+                    '<input type="text" style="width:60px" name="dec_target" id="dec_target" class="dec_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="dec_actual" id="dec_actual" class="dec_actual" disabled>' +
+                    '</td>' + '<td class="jan">' +
+                    '<input type="text" style="width:60px" name="jan_target" id="jan_target" class="jan_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="jan_actual" id="jan_actual" class="jan_actual" disabled>' +
+                    '</td>' + '<td class="feb">' +
+                    '<input type="text" style="width:60px" name="feb_target" id="feb_target" class="feb_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="feb_actual" id="feb_actual" class="feb_actual" disabled>' +
+                    '</td>' + '<td class="march">' +
+                    '<input type="text" style="width:60px" name="march_target" id="march_target" class="march_target">' +
+                    '</td>' + '<td>' +
+                    '<input type="text" style="width:60px" name="march_actual" id="march_actual" class="march_actual" disabled>' +
+                    '</td>' +'<td>'+'<input type="text" style="width:60px" id="total_target" name="total_target"> '+'</td>'+ '</tr>');
             });
-            $('td[name="total"]').append(
-                '<input type="text" style="width:60px" name="total" id="total" >'
+            $('tbody[name="abc"]').append(
+                '<tr>' + '<td>' + 'Total' + '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="aprtarget_total" id="aprtarget_total" >' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="apractual_total" id="apractual_total" disabled>' +
+                '<td>' +
+                '<input type="text" style="width:60px" name="maytarget_total" id="maytarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="mayactual_total" id="mayactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="junetarget_total" id="junetarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="juneactual_total" id="juneactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="julytarget_total" id="julytarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="julyactual_total" id="julyactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="augtarget_total" id="augtarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="augactual_total" id="augactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="septarget_total" id="septarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="sepactual_total" id="sepactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="octtarget_total" id="octtarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="octactual_total" id="octactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="novtarget_total" id="novtarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="novactual_total" id="novactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="dectarget_total" id="dectarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="decactual_total" id="decactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="jantarget_total" id="jantarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="janactual_total" id="janactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="febtarget_total" id="febtarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="febactual_total" id="febactual_total" disabled>' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="marchtarget_total" id="marchtarget_total">' +
+                '</td>' + '<td>' +
+                '<input type="text" style="width:60px" name="marchactual_total" id="marchactual_total" disabled>' +
+                '</td>' + '</tr>'
             );
         },
     });
-     $('input[type=text]').each(function(i, item) {
-         var grade =  $(item).val();
-         alert(grade);
-     });
 });
-// $('#text').keyup(function () {
-//     var parentRow = $(this).parents('tr');
-//     // console.log(parentRow)
-//     var inputs = parentRow.find('input');
-//     // console.log(inputs);
-//     var total = parentRow.find('#total');
-//     // console.log(total);
-//     var sum = 0;    
-//     inputs.each(function () {
-//         sum += parseInt(this.value);
-//     });
-//     sum += inputs.length;
-//     console.log(sum);
-//     total.text(sum);
+var sum = 0;
 
-
-
-$('#text').keyup(function() {
-    $('input[type=text]').each(function(i, item) {
-        var grade = $(item).val();
-        console.log(grade);
+//April
+$(document).on('keyup', '.apt', function() {
+    var sum = 0;
+    $('.apr_target').each(function() {
+        sum += Number($(this).val());
+        $('#aprtarget_total').val(sum);
+        console.log(sum);
     });
-    
 });
-// });
-
+//May
+$(document).on('keyup', '.may', function() {
+    var sum = 0;
+    $('.may_target').each(function() {
+        sum += Number($(this).val());
+        $('#maytarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//June
+$(document).on('keyup', '.june', function() {
+    var sum = 0;
+    $('.june_target').each(function() {
+        sum += Number($(this).val());
+        $('#junetarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//July
+$(document).on('keyup', '.july', function() {
+    var sum = 0;
+    $('.july_target').each(function() {
+        sum += Number($(this).val());
+        $('#julytarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//August
+$(document).on('keyup', '.aug', function() {
+    var sum = 0;
+    $('.aug_target').each(function() {
+        sum += Number($(this).val());
+        $('#augtarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//September
+$(document).on('keyup', '.sept', function() {
+    var sum = 0;
+    $('.sept_target').each(function() {
+        sum += Number($(this).val());
+        $('#septarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//October
+$(document).on('keyup', '.oct', function() {
+    var sum = 0;
+    $('.oct_target').each(function() {
+        sum += Number($(this).val());
+        $('#octtarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//November
+$(document).on('keyup', '.nov', function() {
+    var sum = 0;
+    $('.nov_target').each(function() {
+        sum += Number($(this).val());
+        $('#novtarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//December
+$(document).on('keyup', '.dec', function() {
+    var sum = 0;
+    $('.dec_target').each(function() {
+        sum += Number($(this).val());
+        $('#dectarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//January
+$(document).on('keyup', '.jan', function() {
+    var sum = 0;
+    $('.jan_target').each(function() {
+        sum += Number($(this).val());
+        $('#jantarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//Feb
+$(document).on('keyup', '.feb', function() {
+    var sum = 0;
+    $('.feb_target').each(function() {
+        sum += Number($(this).val());
+        $('#febtarget_total').val(sum);
+        console.log(sum);
+    });
+});
+//March
+$(document).on('keyup', '.march', function() {
+    var sum = 0;
+    $('.march_target').each(function() {
+        sum += Number($(this).val());
+        $('#marchtarget_total').val(sum);
+        console.log(sum);
+    });
+});
 </script>
 @endpush
